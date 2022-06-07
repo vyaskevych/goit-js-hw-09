97313c37862a5f4aa770edb1d533714c1dc2726d
Yuliia Yaskevych <yulia.yaskevych@gmail.com>
2022-06-07 19:48:24 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import "notiflix/dist/notiflix-3.2.5.min.css";
+
+const form = document.querySelector(".form");
+
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let { delay, step, amount } = form.elements;
+  delay = parseInt(delay.value);
+  step = parseInt(step.value);
+  amount = parseInt(amount.value);
+  //[...Array(amount)].map()
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
+
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+
+
